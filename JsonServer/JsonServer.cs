f3a0d33bfd64f6fe65398b30e0e079bbed77b8dc
queens kisivuli <queenskisivuli@gmail.com>
2023-08-16 12:32:40 +0300

    crud car controlles

--- JsonServer/JsonServer.cs
+++ JsonServer/JsonServer.cs
@@ -9,26 +9,56 @@ using car.models;
 namespace car.Services
 {
     public class JsonServer
+{
+    private readonly HttpClient _httpClient;
+
+    public JsonServer(string baseUrl)
     {
-        private readonly HttpClient _httpClient;
-
-        public JsonServer(string baseUrl, string apiKey)
-        {
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri(baseUrl);
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
-        }
-
-        public async Task<bool> SendCarToServerAsync(Car car)
-        {
-            var json = JsonSerializer.Serialize(car);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.PostAsync("cars", content);
-
-            return response.IsSuccessStatusCode;
-        }
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri(baseUrl);
+        // _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
     }
+
+    public async Task<bool> CreateCarAsync(Car car)
+    {
+        var json = JsonSerializer.Serialize(car);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("cars", content);
+
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<Car> GetCarAsync(string id)
+    {
+        var response = await _httpClient.GetAsync($"cars/{id}");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Car>(json);
+        }
+
+        return null;
+    }
+
+    public async Task<bool> UpdateCarAsync(string id, Car car)
+    {
+        var json = JsonSerializer.Serialize(car);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PutAsync($"cars/{id}", content);
+
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> DeleteCarAsync(string id)
+    {
+        var response = await _httpClient.DeleteAsync($"cars/{id}");
+
+        return response.IsSuccessStatusCode;
+    }
+}
 
 }
 
