818c8e63b20f31b65728c48653ebab8239fc8109
queens kisivuli <queenskisivuli@gmail.com>
2023-08-16 11:29:56 +0300

    programm entry and init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using car.Controller;
+using car.models;
+using car.Services;
+using car.views;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string baseUrl = "http://your-json-server-url/";
+        string apiKey = "your-api-key";
+
+        CarView view = new CarView();
+        JsonServer jsonServer = new JsonServer(baseUrl, apiKey);
+        CarController controller = new CarController(view, jsonServer);
+
+        await controller.RunAsync();
+    }
+}
