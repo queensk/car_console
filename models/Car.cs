4144ffb342c1f635be5b07864c1e490066c519cf
queens kisivuli <queenskisivuli@gmail.com>
2023-08-16 11:32:24 +0300

    user models

---
+++ models/Car.cs
@@ -0,0 +1,10 @@
+namespace car.models
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+    }
+}
